01e42732b7d74dfa67a48f4998e890b4cf2520fb
the-pranay <pranaysangolkar533@gmail.com>
2024-06-12 16:44:39 +0530

    24 video completed

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,23 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        this is news component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>NewsApp - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-3">
+             <NewsItem title="myTitle_1" description="myDscr_1"/>
+          </div>
+          <div className="col-md-3">
+             <NewsItem title="myTitle_1" description="myDscr_1"/>
+          </div>
+          <div className="col-md-3">
+             <NewsItem title="myTitle_1" description="myDscr_1"/>
+          </div>
+          <div className="col-md-3">
+             <NewsItem title="myTitle_1" description="myDscr_1"/>
+          </div>
+        </div>
+
       </div>
     )
   }
