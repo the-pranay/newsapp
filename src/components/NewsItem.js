30b16d3ca0f2edbe5f71b91273d9b0c67fa11763
the-pranay <pranaysangolkar533@gmail.com>
2024-06-14 20:55:40 +0530

    27 video completed

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,9 +6,9 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://images.moneycontrol.com/static-mcnews/2024/06/20240612102432_sensex_nifty-market.jpg":imageUrl } className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+            <h5 className="card-title">{title}...</h5> 
             <p className="card-text">{description}...</p>
             <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
                Read More
